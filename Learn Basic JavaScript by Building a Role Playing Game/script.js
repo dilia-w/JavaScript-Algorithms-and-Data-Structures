f585e048544751f477a783dc947e0819be563ff6
dilia-w <72775740+dilia-w@users.noreply.github.com>
2024-07-14 17:39:42 -0300

    Update script.js

--- Learn Basic JavaScript by Building a Role Playing Game/script.js
+++ Learn Basic JavaScript by Building a Role Playing Game/script.js
@@ -1,372 +1,288 @@
-const vertexShaderSource = `#version 300 es
-#pragma vscode_glsllintstage: vert
+let xp = 0;
+let health = 100;
+let gold = 50;
+let currentWeapon = 0;
+let fighting;
+let monsterHealth;
+let inventory = ["stick"];
 
-in float aPointSize;
-in vec2 aPosition;
-in vec4 aColor;
+const button1 = document.querySelector('#button1');
+const button2 = document.querySelector("#button2");
+const button3 = document.querySelector("#button3");
+const text = document.querySelector("#text");
+const xpText = document.querySelector("#xpText");
+const healthText = document.querySelector("#healthText");
+const goldText = document.querySelector("#goldText");
+const monsterStats = document.querySelector("#monsterStats");
+const monsterName = document.querySelector("#monsterName");
+const monsterHealthText = document.querySelector("#monsterHealth");
+const weapons = [
+  { name: 'stick', power: 5 },
+  { name: 'dagger', power: 30 },
+  { name: 'claw hammer', power: 50 },
+  { name: 'sword', power: 100 }
+];
+const monsters = [
+  {
+    name: "slime",
+    level: 2,
+    health: 15
+  },
+  {
+    name: "fanged beast",
+    level: 8,
+    health: 60
+  },
+  {
+    name: "dragon",
+    level: 20,
+    health: 300
+  }
+]
+const locations = [
+  {
+    name: "town square",
+    "button text": ["Go to store", "Go to cave", "Fight dragon"],
+    "button functions": [goStore, goCave, fightDragon],
+    text: "You are in the town square. You see a sign that says \"Store\"."
+  },
+  {
+    name: "store",
+    "button text": ["Buy 10 health (10 gold)", "Buy weapon (30 gold)", "Go to town square"],
+    "button functions": [buyHealth, buyWeapon, goTown],
+    text: "You enter the store."
+  },
+  {
+    name: "cave",
+    "button text": ["Fight slime", "Fight fanged beast", "Go to town square"],
+    "button functions": [fightSlime, fightBeast, goTown],
+    text: "You enter the cave. You see some monsters."
+  },
+  {
+    name: "fight",
+    "button text": ["Attack", "Dodge", "Run"],
+    "button functions": [attack, dodge, goTown],
+    text: "You are fighting a monster."
+  },
+  {
+    name: "kill monster",
+    "button text": ["Go to town square", "Go to town square", "Go to town square"],
+    "button functions": [goTown, goTown, easterEgg],
+    text: 'The monster screams "Arg!" as it dies. You gain experience points and find gold.'
+  },
+  {
+    name: "lose",
+    "button text": ["REPLAY?", "REPLAY?", "REPLAY?"],
+    "button functions": [restart, restart, restart],
+    text: "You die. &#x2620;"
+  },
+  { 
+    name: "win", 
+    "button text": ["REPLAY?", "REPLAY?", "REPLAY?"], 
+    "button functions": [restart, restart, restart], 
+    text: "You defeat the dragon! YOU WIN THE GAME! &#x1F389;" 
+  },
+  {
+    name: "easter egg",
+    "button text": ["2", "8", "Go to town square?"],
+    "button functions": [pickTwo, pickEight, goTown],
+    text: "You find a secret game. Pick a number above. Ten numbers will be randomly chosen between 0 and 10. If the number you choose matches one of the random numbers, you win!"
+  }
+];
 
-uniform mat4 uTransformMatrix;
+// initialize buttons
+button1.onclick = goStore;
+button2.onclick = goCave;
+button3.onclick = fightDragon;
 
-out vec4 vColor;
-
-void main()
-{
-    gl_PointSize = aPointSize;
-    gl_Position = uTransformMatrix * vec4(aPosition, 0.0, 1.0);
-    vColor = aColor;
-}`;
-
-const fragmentShaderSource = `#version 300 es
-#pragma vscode_glsllintstage: frag
-
-precision mediump float;
-
-in vec4 vColor;
-
-out vec4 fragColor;
-
-void main()
-{
-    fragColor = vColor;
-}`;
-
-const canvas = document.querySelector('canvas');
-const gl = canvas.getContext('webgl2');
-
-const program = gl.createProgram();
-
-const vertexShader = gl.createShader(gl.VERTEX_SHADER);
-gl.shaderSource(vertexShader, vertexShaderSource);
-gl.compileShader(vertexShader);
-gl.attachShader(program, vertexShader);
-
-const fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
-gl.shaderSource(fragmentShader, fragmentShaderSource);
-gl.compileShader(fragmentShader);
-gl.attachShader(program, fragmentShader);
-
-gl.linkProgram(program);
-
-if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
-    console.log(gl.getShaderInfoLog(vertexShader));
-    console.log(gl.getShaderInfoLog(fragmentShader));
+function update(location) {
+  monsterStats.style.display = "none";
+  button1.innerText = location["button text"][0];
+  button2.innerText = location["button text"][1];
+  button3.innerText = location["button text"][2];
+  button1.onclick = location["button functions"][0];
+  button2.onclick = location["button functions"][1];
+  button3.onclick = location["button functions"][2];
+  text.innerHTML = location.text;
 }
 
-gl.useProgram(program);
+function goTown() {
+  update(locations[0]);
+}
 
-const aPositionLoc = gl.getAttribLocation(program, 'aPosition');
-const aPointSizeLoc = gl.getAttribLocation(program, 'aPointSize');
-const aColorLoc = gl.getAttribLocation(program, 'aColor');
-const uOffsetLoc = gl.getUniformLocation(program, 'uOffset');
+function goStore() {
+  update(locations[1]);
+}
 
-const uTransformMatrixLoc = gl.getUniformLocation(program, 'uTransformMatrix');
+function goCave() {
+  update(locations[2]);
+}
 
+function buyHealth() {
+  if (gold >= 10) {
+    gold -= 10;
+    health += 10;
+    goldText.innerText = gold;
+    healthText.innerText = health;
+  } else {
+    text.innerText = "You do not have enough gold to buy health.";
+  }
+}
 
-
-const bufferData = new Float32Array([
-    // Coordenadas/////////////////////////////////
-    // left column
-    0, 0,
-    .3, .0,
-    .0, 1.5,
-    
-    .3, 1.5,
-    .3, .0,
-    .0, 1.5,    
-
-    // top rung
-    .3, 1.2,
-    1, 1.2,
-    .3, 1.5,
-
-    1, 1.5,    
-    1, 1.2,
-    .3, 1.5,
-
-    // middle rung
-    .3, .6,
-    .67, .6,
-    .3, .9,
-
-    .67, .9,    
-    .67, .6,
-    .3, .9,
-    // Coordenadas
-    // left column
-    0, 0,
-    .3, .0,
-    .0, 1.5,
-    
-    .3, 1.5,
-    .3, .0,
-    .0, 1.5,    
-
-    // top rung
-    .3, 0,
-    1, 0,
-    .3, .3,
-
-    1, .3,    
-    1, 0,
-    .3, .3,    
-
-    // Tamanho/////////////////////////////////////////
-    100.0,
-    30.0,
-    50.0,
-
-    100.0,
-    30.0,
-    50.0,
-
-    100.0,
-    30.0,
-    50.0,
-
-    100.0,
-    30.0,
-    50.0,
-
-    100.0,
-    30.0,
-    50.0,
-
-    100.0,
-    30.0,
-    50.0,    
-
-    // Tamanho
-    100.0,
-    30.0,
-    50.0,
-
-    100.0,
-    30.0,
-    50.0,
-
-    100.0,
-    30.0,
-    50.0,
-
-    100.0,
-    30.0,
-    50.0,
-
-    // Cor//////////////////////////////////////////////
-    1.0, 0.0, 0.0, 1.0, // Vermelho
-    0.0, 1.0, 0.0, 1.0, // Verde
-    0.0, 0.0, 1.0, 1.0,  // Azul
-
-    1.0, 0.0, 0.0, 1.0, // Vermelho
-    0.0, 1.0, 0.0, 1.0, // Verde
-    0.0, 0.0, 1.0, 1.0,  // Azul
-
-    1.0, 0.0, 0.0, 1.0, // Vermelho
-    0.0, 1.0, 0.0, 1.0, // Verde
-    0.0, 0.0, 1.0, 1.0,  // Azul
-
-    1.0, 0.0, 0.0, 1.0, // Vermelho
-    0.0, 1.0, 0.0, 1.0, // Verde
-    0.0, 0.0, 1.0, 1.0,  // Azul
-
-    1.0, 0.0, 0.0, 1.0, // Vermelho
-    0.0, 1.0, 0.0, 1.0, // Verde
-    0.0, 0.0, 1.0, 1.0,  // Azul
-
-    1.0, 0.0, 0.0, 1.0, // Vermelho
-    0.0, 1.0, 0.0, 1.0, // Verde
-    0.0, 0.0, 1.0, 1.0,  // Azul   
-    
-    // Cor
-    1.0, 1.0, 0.0, 1.0, // Amarelo
-    0.0, 1.0, 1.0, 1.0, // Ciano
-    1.0, 0.0, 1.0, 1.0,  // Magenta
-
-    1.0, 1.0, 0.0, 1.0, // Amarelo
-    0.0, 1.0, 1.0, 1.0, // Ciano
-    1.0, 0.0, 1.0, 1.0,  // Magenta
-
-    1.0, 1.0, 0.0, 1.0, // Amarelo
-    0.0, 1.0, 1.0, 1.0, // Ciano
-    1.0, 0.0, 1.0, 1.0,  // Magenta
-
-    1.0, 1.0, 0.0, 1.0, // Amarelo
-    0.0, 1.0, 1.0, 1.0, // Ciano
-    1.0, 0.0, 1.0, 1.0,  // Magenta  
-   
-]);
-
-const buffer = gl.createBuffer();
-gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
-gl.bufferData(gl.ARRAY_BUFFER, bufferData, gl.STATIC_DRAW);
-
-gl.enableVertexAttribArray(aPositionLoc);
-gl.enableVertexAttribArray(aPointSizeLoc);
-gl.enableVertexAttribArray(aColorLoc);
-
-gl.vertexAttribPointer(aPositionLoc, 2, gl.FLOAT, false, 0, 0);
-gl.vertexAttribPointer(aPointSizeLoc, 1, gl.FLOAT, false, 0, 60 * 4);
-gl.vertexAttribPointer(aColorLoc, 4, gl.FLOAT, false, 0, 90 * 4);
-
-const ySlider = document.getElementById('ySlider');
-const Angulo = document.getElementById('Angulo');
-const escala = document.getElementById('escala');
-const startButton = document.getElementById('startButton');
-
-let startTime = null;
-requestAnimationFrame(drawScene);//pra começar ja
-
-/////////////////////////////////////////////////////////////////////////////////////////////
-
-function drawScene(timestamp) {
-
-    const v2 = 3.39; //diferença de frequencia em relação ao primeiro
-    const v3 = 7.12;
-    if (!startTime) startTime = timestamp;
-    const elapsedTime = (timestamp - startTime) / 1000; // in seconds
-
-    const cycleTime = 1; // x seconds to go and x to return
-    const totalCycle = cycleTime * 2;
-
-    let t = elapsedTime % totalCycle;
-    if (t > cycleTime) {
-        t = totalCycle - t;       
-    } 
-    var variacao = ((t / cycleTime) * 2 - 1);
-    
-    let t2 = elapsedTime % (v2*totalCycle);
-    if (t2 > v2*cycleTime) {
-        t2 = v2*totalCycle - t2;       
+function buyWeapon() {
+  if (currentWeapon < weapons.length - 1) {
+    if (gold >= 30) {
+      gold -= 30;
+      currentWeapon++;
+      goldText.innerText = gold;
+      let newWeapon = weapons[currentWeapon].name;
+      text.innerText = "You now have a " + newWeapon + ".";
+      inventory.push(newWeapon);
+      text.innerText += " In your inventory you have: " + inventory;
+    } else {
+      text.innerText = "You do not have enough gold to buy a weapon.";
     }
-
-    let t3 = elapsedTime % (v3*totalCycle);
-    if (t3 > v3*cycleTime) {
-        t3 = v3*totalCycle - t3;       
-    }        
-    var variacao2 = ((t2 / (v2*cycleTime)) * 2 - 1);
-    var variacao3 = ((t3 / (v3*cycleTime)) * 2 - 1);
-
-    const xOffset = variacao ; 
-    const yOffset = parseFloat(ySlider.value);
-    const xOffset2 = variacao2; 
-    const yOffset2 = parseFloat(ySlider.value);
-    const xOffset3 = variacao3; 
-    const yOffset3 = parseFloat(ySlider.value);
-
-    gl.clear(gl.COLOR_BUFFER_BIT);
-
-    // Definindo a matriz de projeção ortográfica
-    const orthoMatrix = createOrthographicMatrix(-3, 3, -3, 3, -1, 1);
-
-    // Criando Matrizes Individuais
-    const scaleMatrix = createScaleMatrix(parseFloat(escala.value), parseFloat(escala.value));
-    const rotationMatrixZ = createRotationMatrixZ(parseFloat(Angulo.value) * Math.PI / 180);
-    const translationMatrix1 = createTranslationMatrix(xOffset, yOffset);
-    const translationMatrix2 = createTranslationMatrix(xOffset2, yOffset2);
-    const translationMatrix3 = createTranslationMatrix(xOffset3, yOffset3);
-
-    // Combinando Matrizes na Ordem Correta
-    const transformMatrix1 = multiplyMatrices(orthoMatrix, multiplyMatrices(rotationMatrixZ, multiplyMatrices(translationMatrix1, scaleMatrix)));
-    const transformMatrix2 = multiplyMatrices(orthoMatrix, multiplyMatrices(translationMatrix2, multiplyMatrices(rotationMatrixZ, scaleMatrix)));
-    const transformMatrix3 = multiplyMatrices(orthoMatrix, multiplyMatrices(translationMatrix3, multiplyMatrices(rotationMatrixZ, scaleMatrix)));
-
-    // Aplicando Matrizes de Transformação e Desenhando    
-
-    gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
-
-    gl.uniformMatrix4fv(uTransformMatrixLoc, false, transformMatrix3);
-    gl.enableVertexAttribArray(aPositionLoc);
-    gl.enableVertexAttribArray(aPointSizeLoc);
-    gl.enableVertexAttribArray(aColorLoc);
-    gl.drawArrays(gl.TRIANGLES, 0, 18);    
-    
-    gl.uniformMatrix4fv(uTransformMatrixLoc, false, transformMatrix2);    
-    gl.drawArrays(gl.TRIANGLES, 0, 18);
-    /////////////////////////////////////////////////////////////////////////////
-    
-    gl.uniformMatrix4fv(uTransformMatrixLoc, false, transformMatrix1);
-    gl.drawArrays(gl.TRIANGLES, 18, 12);
-    
-    requestAnimationFrame(drawScene);
+  } else {
+    text.innerText = "You already have the most powerful weapon!";
+    button2.innerText = "Sell weapon for 15 gold";
+    button2.onclick = sellWeapon;
+  }
 }
 
-startButton.addEventListener('click', () => {
-    startTime = null; // Reset the start time
-    requestAnimationFrame(drawScene);
-});
+function sellWeapon() {
+  if (inventory.length > 1) {
+    gold += 15;
+    goldText.innerText = gold;
+    let currentWeapon = inventory.shift();
+    text.innerText = "You sold a " + currentWeapon + ".";
+    text.innerText += " In your inventory you have: " + inventory;
+  } else {
+    text.innerText = "Don't sell your only weapon!";
+  }
+}
 
-function createTranslationMatrix(tx, ty) {
-    return new Float32Array([
-        1, 0, 0, 0,
-        0, 1, 0, 0,
-        0, 0, 1, 0,
-        tx, ty, 0, 1
-    ]);
+function fightSlime() {
+  fighting = 0;
+  goFight();
 }
-/////////////////////////////////////////////////////////////////////////////////////////////
-function createRotationMatrixX(angleInRadians) {
-    const xcos = Math.cos(angleInRadians);
-    const xsin = Math.sin(angleInRadians);
-    return new Float32Array([
-        1, 0,     0,    0,
-        0, xcos, -xsin, 0,
-        0, xsin,  xcos, 0,
-        0, 0,     0,    1
-    ]);
+
+function fightBeast() {
+  fighting = 1;
+  goFight();
 }
-/////////////////////////////////////////////////////////////////////////////////////////////
-function createRotationMatrixY(angleInRadians) {
-    const ycos = Math.cos(angleInRadians);
-    const ysin = Math.sin(angleInRadians);
-    return new Float32Array([
-        ycos,  0,  ysin, 0,
-        0,     1,  0,    0,
-        -ysin, 0,  ycos, 0,
-        0,     0,  0,    1
-    ]);
+
+function fightDragon() {
+  fighting = 2;
+  goFight();
 }
-/////////////////////////////////////////////////////////////////////////////////////////////
-function createRotationMatrixZ(angleInRadians) {
-    const zcos = Math.cos(angleInRadians);
-    const zsin = Math.sin(angleInRadians);
-    return new Float32Array([
-        zcos, -zsin, 0, 0,
-        zsin, zcos,  0, 0,
-        0,   0,    1, 0,
-        0,   0,    0, 1
-    ]);
+
+function goFight() {
+  update(locations[3]);
+  monsterHealth = monsters[fighting].health;
+  monsterStats.style.display = "block";
+  monsterName.innerText = monsters[fighting].name;
+  monsterHealthText.innerText = monsterHealth;
 }
-/////////////////////////////////////////////////////////////////////////////////////////////
-function createScaleMatrix(sx, sy) {
-    return new Float32Array([
-        sx, 0,  0, 0,
-        0, sy,  0, 0,
-        0, 0,  1, 0,
-        0, 0,  0, 1
-    ]);
-}
-/////////////////////////////////////////////////////////////////////////////////////////////
-function createOrthographicMatrix(left, right, bottom, top, near, far) {
-    return new Float32Array([
-        2 / (right - left), 0, 0, 0,
-        0, 2 / (top - bottom), 0, 0,
-        0, 0, -2 / (far - near), 0,
-        -(right + left) / (right - left), -(top + bottom) / (top - bottom), -(far + near) / (far - near), 1
-    ]);
-}
-/////////////////////////////////////////////////////////////////////////////////////////////
-function multiplyMatrices(a, b) {
-    const result = new Float32Array(16);
-    for (let row = 0; row < 4; row++) {
-        for (let col = 0; col < 4; col++) {
-            result[row * 4 + col] =
-                a[row * 4 + 0] * b[0 * 4 + col] +
-                a[row * 4 + 1] * b[1 * 4 + col] +
-                a[row * 4 + 2] * b[2 * 4 + col] +
-                a[row * 4 + 3] * b[3 * 4 + col];
-        }
+
+function attack() {
+  text.innerText = "The " + monsters[fighting].name + " attacks.";
+  text.innerText += " You attack it with your " + weapons[currentWeapon].name + ".";
+  health -= getMonsterAttackValue(monsters[fighting].level);
+  if (isMonsterHit()) {
+    monsterHealth -= weapons[currentWeapon].power + Math.floor(Math.random() * xp) + 1;    
+  } else {
+    text.innerText += " You miss.";
+  }
+  healthText.innerText = health;
+  monsterHealthText.innerText = monsterHealth;
+  if (health <= 0) {
+    lose();
+  } else if (monsterHealth <= 0) {
+    if (fighting === 2) {
+      winGame();
+    } else {
+      defeatMonster();
     }
+  }
+  if (Math.random() <= .1 && inventory.length !== 1) {
+    text.innerText += " Your " + inventory.pop() + " breaks.";
+    currentWeapon--;
+  }
+}
 
-    return result;
-    
+function getMonsterAttackValue(level) {
+  const hit = (level * 5) - (Math.floor(Math.random() * xp));
+  console.log(hit);
+  return hit > 0 ? hit : 0;
+}
+
+function isMonsterHit() {
+  return Math.random() > .2 || health < 20;
+}
+
+function dodge() {
+  text.innerText = "You dodge the attack from the " + monsters[fighting].name;
+}
+
+function defeatMonster() {
+  gold += Math.floor(monsters[fighting].level * 6.7);
+  xp += monsters[fighting].level;
+  goldText.innerText = gold;
+  xpText.innerText = xp;
+  update(locations[4]);
+}
+
+function lose() {
+  update(locations[5]);
+}
+
+function winGame() {
+  update(locations[6]);
+}
+
+function restart() {
+  xp = 0;
+  health = 100;
+  gold = 50;
+  currentWeapon = 0;
+  inventory = ["stick"];
+  goldText.innerText = gold;
+  healthText.innerText = health;
+  xpText.innerText = xp;
+  goTown();
+}
+
+function easterEgg() {
+  update(locations[7]);
+}
+
+function pickTwo() {
+  pick(2);
+}
+
+function pickEight() {
+  pick(8);
+}
+
+function pick(guess) {
+  const numbers = [];
+  while (numbers.length < 10) {
+    numbers.push(Math.floor(Math.random() * 11));
+  }
+  text.innerText = "You picked " + guess + ". Here are the random numbers:\n";
+  for (let i = 0; i < 10; i++) {
+    text.innerText += numbers[i] + "\n";
+  }
+  if (numbers.includes(guess)) {
+    text.innerText += "Right! You win 20 gold!";
+    gold += 20;
+    goldText.innerText = gold;
+  } else {
+    text.innerText += "Wrong! You lose 10 health!";
+    health -= 10;
+    healthText.innerText = health;
+    if (health <= 0) {
+      lose();
+    }
+  }
 }
