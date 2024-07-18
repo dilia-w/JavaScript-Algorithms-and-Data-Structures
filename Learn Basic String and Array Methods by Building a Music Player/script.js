244435d932a4b507cfc42d752a79b75f5f03e804
dilia-w <72775740+dilia-w@users.noreply.github.com>
2024-07-18 15:40:42 -0300

    Update script.js

--- Learn Basic String and Array Methods by Building a Music Player/script.js
+++ Learn Basic String and Array Methods by Building a Music Player/script.js
@@ -1,372 +1,286 @@
-const vertexShaderSource = `#version 300 es
-#pragma vscode_glsllintstage: vert
-
-in float aPointSize;
-in vec2 aPosition;
-in vec4 aColor;
-
-uniform mat4 uTransformMatrix;
-
-out vec4 vColor;
-
-void main()
-{
-    gl_PointSize = aPointSize;
-    gl_Position = uTransformMatrix * vec4(aPosition, 0.0, 1.0);
-    vColor = aColor;
-}`;
-
-const fragmentShaderSource = `#version 300 es
-#pragma vscode_glsllintstage: frag
-
-precision mediump float;
-
-in vec4 vColor;
-
-out vec4 fragColor;
-
-void main()
-{
-    fragColor = vColor;
-}`;
-
-const canvas = document.querySelector('canvas');
-const gl = canvas.getContext('webgl2');
-
-const program = gl.createProgram();
-
-const vertexShader = gl.createShader(gl.VERTEX_SHADER);
-gl.shaderSource(vertexShader, vertexShaderSource);
-gl.compileShader(vertexShader);
-gl.attachShader(program, vertexShader);
-
-const fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
-gl.shaderSource(fragmentShader, fragmentShaderSource);
-gl.compileShader(fragmentShader);
-gl.attachShader(program, fragmentShader);
-
-gl.linkProgram(program);
-
-if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
-    console.log(gl.getShaderInfoLog(vertexShader));
-    console.log(gl.getShaderInfoLog(fragmentShader));
-}
-
-gl.useProgram(program);
-
-const aPositionLoc = gl.getAttribLocation(program, 'aPosition');
-const aPointSizeLoc = gl.getAttribLocation(program, 'aPointSize');
-const aColorLoc = gl.getAttribLocation(program, 'aColor');
-const uOffsetLoc = gl.getUniformLocation(program, 'uOffset');
-
-const uTransformMatrixLoc = gl.getUniformLocation(program, 'uTransformMatrix');
-
-
-
-const bufferData = new Float32Array([
-    // Coordenadas/////////////////////////////////
-    // left column
-    0, 0,
-    .3, .0,
-    .0, 1.5,
-    
-    .3, 1.5,
-    .3, .0,
-    .0, 1.5,    
-
-    // top rung
-    .3, 1.2,
-    1, 1.2,
-    .3, 1.5,
-
-    1, 1.5,    
-    1, 1.2,
-    .3, 1.5,
-
-    // middle rung
-    .3, .6,
-    .67, .6,
-    .3, .9,
-
-    .67, .9,    
-    .67, .6,
-    .3, .9,
-    // Coordenadas
-    // left column
-    0, 0,
-    .3, .0,
-    .0, 1.5,
-    
-    .3, 1.5,
-    .3, .0,
-    .0, 1.5,    
-
-    // top rung
-    .3, 0,
-    1, 0,
-    .3, .3,
-
-    1, .3,    
-    1, 0,
-    .3, .3,    
-
-    // Tamanho/////////////////////////////////////////
-    100.0,
-    30.0,
-    50.0,
-
-    100.0,
-    30.0,
-    50.0,
-
-    100.0,
-    30.0,
-    50.0,
-
-    100.0,
-    30.0,
-    50.0,
-
-    100.0,
-    30.0,
-    50.0,
-
-    100.0,
-    30.0,
-    50.0,    
-
-    // Tamanho
-    100.0,
-    30.0,
-    50.0,
-
-    100.0,
-    30.0,
-    50.0,
-
-    100.0,
-    30.0,
-    50.0,
-
-    100.0,
-    30.0,
-    50.0,
-
-    // Cor//////////////////////////////////////////////
-    1.0, 0.0, 0.0, 1.0, // Vermelho
-    0.0, 1.0, 0.0, 1.0, // Verde
-    0.0, 0.0, 1.0, 1.0,  // Azul
-
-    1.0, 0.0, 0.0, 1.0, // Vermelho
-    0.0, 1.0, 0.0, 1.0, // Verde
-    0.0, 0.0, 1.0, 1.0,  // Azul
-
-    1.0, 0.0, 0.0, 1.0, // Vermelho
-    0.0, 1.0, 0.0, 1.0, // Verde
-    0.0, 0.0, 1.0, 1.0,  // Azul
-
-    1.0, 0.0, 0.0, 1.0, // Vermelho
-    0.0, 1.0, 0.0, 1.0, // Verde
-    0.0, 0.0, 1.0, 1.0,  // Azul
-
-    1.0, 0.0, 0.0, 1.0, // Vermelho
-    0.0, 1.0, 0.0, 1.0, // Verde
-    0.0, 0.0, 1.0, 1.0,  // Azul
-
-    1.0, 0.0, 0.0, 1.0, // Vermelho
-    0.0, 1.0, 0.0, 1.0, // Verde
-    0.0, 0.0, 1.0, 1.0,  // Azul   
-    
-    // Cor
-    1.0, 1.0, 0.0, 1.0, // Amarelo
-    0.0, 1.0, 1.0, 1.0, // Ciano
-    1.0, 0.0, 1.0, 1.0,  // Magenta
-
-    1.0, 1.0, 0.0, 1.0, // Amarelo
-    0.0, 1.0, 1.0, 1.0, // Ciano
-    1.0, 0.0, 1.0, 1.0,  // Magenta
-
-    1.0, 1.0, 0.0, 1.0, // Amarelo
-    0.0, 1.0, 1.0, 1.0, // Ciano
-    1.0, 0.0, 1.0, 1.0,  // Magenta
-
-    1.0, 1.0, 0.0, 1.0, // Amarelo
-    0.0, 1.0, 1.0, 1.0, // Ciano
-    1.0, 0.0, 1.0, 1.0,  // Magenta  
-   
-]);
-
-const buffer = gl.createBuffer();
-gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
-gl.bufferData(gl.ARRAY_BUFFER, bufferData, gl.STATIC_DRAW);
-
-gl.enableVertexAttribArray(aPositionLoc);
-gl.enableVertexAttribArray(aPointSizeLoc);
-gl.enableVertexAttribArray(aColorLoc);
-
-gl.vertexAttribPointer(aPositionLoc, 2, gl.FLOAT, false, 0, 0);
-gl.vertexAttribPointer(aPointSizeLoc, 1, gl.FLOAT, false, 0, 60 * 4);
-gl.vertexAttribPointer(aColorLoc, 4, gl.FLOAT, false, 0, 90 * 4);
-
-const ySlider = document.getElementById('ySlider');
-const Angulo = document.getElementById('Angulo');
-const escala = document.getElementById('escala');
-const startButton = document.getElementById('startButton');
-
-let startTime = null;
-requestAnimationFrame(drawScene);//pra começar ja
-
-/////////////////////////////////////////////////////////////////////////////////////////////
-
-function drawScene(timestamp) {
-
-    const v2 = 3.39; //diferença de frequencia em relação ao primeiro
-    const v3 = 7.12;
-    if (!startTime) startTime = timestamp;
-    const elapsedTime = (timestamp - startTime) / 1000; // in seconds
-
-    const cycleTime = 1; // x seconds to go and x to return
-    const totalCycle = cycleTime * 2;
-
-    let t = elapsedTime % totalCycle;
-    if (t > cycleTime) {
-        t = totalCycle - t;       
-    } 
-    var variacao = ((t / cycleTime) * 2 - 1);
-    
-    let t2 = elapsedTime % (v2*totalCycle);
-    if (t2 > v2*cycleTime) {
-        t2 = v2*totalCycle - t2;       
-    }
-
-    let t3 = elapsedTime % (v3*totalCycle);
-    if (t3 > v3*cycleTime) {
-        t3 = v3*totalCycle - t3;       
-    }        
-    var variacao2 = ((t2 / (v2*cycleTime)) * 2 - 1);
-    var variacao3 = ((t3 / (v3*cycleTime)) * 2 - 1);
-
-    const xOffset = variacao ; 
-    const yOffset = parseFloat(ySlider.value);
-    const xOffset2 = variacao2; 
-    const yOffset2 = parseFloat(ySlider.value);
-    const xOffset3 = variacao3; 
-    const yOffset3 = parseFloat(ySlider.value);
-
-    gl.clear(gl.COLOR_BUFFER_BIT);
-
-    // Definindo a matriz de projeção ortográfica
-    const orthoMatrix = createOrthographicMatrix(-3, 3, -3, 3, -1, 1);
-
-    // Criando Matrizes Individuais
-    const scaleMatrix = createScaleMatrix(parseFloat(escala.value), parseFloat(escala.value));
-    const rotationMatrixZ = createRotationMatrixZ(parseFloat(Angulo.value) * Math.PI / 180);
-    const translationMatrix1 = createTranslationMatrix(xOffset, yOffset);
-    const translationMatrix2 = createTranslationMatrix(xOffset2, yOffset2);
-    const translationMatrix3 = createTranslationMatrix(xOffset3, yOffset3);
-
-    // Combinando Matrizes na Ordem Correta
-    const transformMatrix1 = multiplyMatrices(orthoMatrix, multiplyMatrices(rotationMatrixZ, multiplyMatrices(translationMatrix1, scaleMatrix)));
-    const transformMatrix2 = multiplyMatrices(orthoMatrix, multiplyMatrices(translationMatrix2, multiplyMatrices(rotationMatrixZ, scaleMatrix)));
-    const transformMatrix3 = multiplyMatrices(orthoMatrix, multiplyMatrices(translationMatrix3, multiplyMatrices(rotationMatrixZ, scaleMatrix)));
-
-    // Aplicando Matrizes de Transformação e Desenhando    
-
-    gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
-
-    gl.uniformMatrix4fv(uTransformMatrixLoc, false, transformMatrix3);
-    gl.enableVertexAttribArray(aPositionLoc);
-    gl.enableVertexAttribArray(aPointSizeLoc);
-    gl.enableVertexAttribArray(aColorLoc);
-    gl.drawArrays(gl.TRIANGLES, 0, 18);    
-    
-    gl.uniformMatrix4fv(uTransformMatrixLoc, false, transformMatrix2);    
-    gl.drawArrays(gl.TRIANGLES, 0, 18);
-    /////////////////////////////////////////////////////////////////////////////
-    
-    gl.uniformMatrix4fv(uTransformMatrixLoc, false, transformMatrix1);
-    gl.drawArrays(gl.TRIANGLES, 18, 12);
-    
-    requestAnimationFrame(drawScene);
-}
-
-startButton.addEventListener('click', () => {
-    startTime = null; // Reset the start time
-    requestAnimationFrame(drawScene);
+const playlistSongs = document.getElementById("playlist-songs");
+const playButton = document.getElementById("play");
+const pauseButton = document.getElementById("pause");
+const nextButton = document.getElementById("next");
+const previousButton = document.getElementById("previous");
+const shuffleButton = document.getElementById("shuffle");
+
+const allSongs = [
+  {
+    id: 0,
+    title: "Scratching The Surface",
+    artist: "Quincy Larson",
+    duration: "4:25",
+    src: "https://cdn.freecodecamp.org/curriculum/js-music-player/scratching-the-surface.mp3",
+  },
+  {
+    id: 1,
+    title: "Can't Stay Down",
+    artist: "Quincy Larson",
+    duration: "4:15",
+    src: "https://cdn.freecodecamp.org/curriculum/js-music-player/can't-stay-down.mp3",
+  },
+  {
+    id: 2,
+    title: "Still Learning",
+    artist: "Quincy Larson",
+    duration: "3:51",
+    src: "https://cdn.freecodecamp.org/curriculum/js-music-player/still-learning.mp3",
+  },
+  {
+    id: 3,
+    title: "Cruising for a Musing",
+    artist: "Quincy Larson",
+    duration: "3:34",
+    src: "https://cdn.freecodecamp.org/curriculum/js-music-player/cruising-for-a-musing.mp3",
+  },
+  {
+    id: 4,
+    title: "Never Not Favored",
+    artist: "Quincy Larson",
+    duration: "3:35",
+    src: "https://cdn.freecodecamp.org/curriculum/js-music-player/never-not-favored.mp3",
+  },
+  {
+    id: 5,
+    title: "From the Ground Up",
+    artist: "Quincy Larson",
+    duration: "3:12",
+    src: "https://cdn.freecodecamp.org/curriculum/js-music-player/from-the-ground-up.mp3",
+  },
+  {
+    id: 6,
+    title: "Walking on Air",
+    artist: "Quincy Larson",
+    duration: "3:25",
+    src: "https://cdn.freecodecamp.org/curriculum/js-music-player/walking-on-air.mp3",
+  },
+  {
+    id: 7,
+    title: "Can't Stop Me. Can't Even Slow Me Down.",
+    artist: "Quincy Larson",
+    duration: "3:52",
+    src: "https://cdn.freecodecamp.org/curriculum/js-music-player/cant-stop-me-cant-even-slow-me-down.mp3",
+  },
+  {
+    id: 8,
+    title: "The Surest Way Out is Through",
+    artist: "Quincy Larson",
+    duration: "3:10",
+    src: "https://cdn.freecodecamp.org/curriculum/js-music-player/the-surest-way-out-is-through.mp3",
+  },
+  {
+    id: 9,
+    title: "Chasing That Feeling",
+    artist: "Quincy Larson",
+    duration: "2:43",
+    src: "https://cdn.freecodecamp.org/curriculum/js-music-player/chasing-that-feeling.mp3",
+  },
+];
+
+const audio = new Audio();
+let userData = {
+  songs: [...allSongs],
+  currentSong: null,
+  songCurrentTime: 0,
+};
+
+const playSong = (id) => {
+  const song = userData?.songs.find((song) => song.id === id);
+  audio.src = song.src;
+  audio.title = song.title;
+
+  if (userData?.currentSong === null || userData?.currentSong.id !== song.id) {
+    audio.currentTime = 0;
+  } else {
+    audio.currentTime = userData?.songCurrentTime;
+  }
+  userData.currentSong = song;
+  playButton.classList.add("playing");
+
+  highlightCurrentSong();
+  setPlayerDisplay();
+  setPlayButtonAccessibleText();
+  audio.play();
+};
+
+const pauseSong = () => {
+  userData.songCurrentTime = audio.currentTime;
+  
+  playButton.classList.remove("playing");
+  audio.pause();
+};
+
+const playNextSong = () => {
+  if (userData?.currentSong === null) {
+    playSong(userData?.songs[0].id);
+  } else {
+    const currentSongIndex = getCurrentSongIndex();
+    const nextSong = userData?.songs[currentSongIndex + 1];
+
+    playSong(nextSong.id);
+  }
+};
+
+const playPreviousSong = () => {
+   if (userData?.currentSong === null) return;
+   else {
+    const currentSongIndex = getCurrentSongIndex();
+    const previousSong = userData?.songs[currentSongIndex - 1];
+
+    playSong(previousSong.id);
+   }
+};
+
+const shuffle = () => {
+  userData?.songs.sort(() => Math.random() - 0.5);
+  userData.currentSong = null;
+  userData.songCurrentTime = 0;
+
+  renderSongs(userData?.songs);
+  pauseSong();
+  setPlayerDisplay();
+  setPlayButtonAccessibleText();
+};
+
+const deleteSong = (id) => {
+  if (userData?.currentSong?.id === id) {
+    userData.currentSong = null;
+    userData.songCurrentTime = 0;
+
+    pauseSong();
+    setPlayerDisplay();
+  }
+
+  userData.songs = userData?.songs.filter((song) => song.id !== id);
+  renderSongs(userData?.songs); 
+  highlightCurrentSong(); 
+  setPlayButtonAccessibleText(); 
+
+  if (userData?.songs.length === 0) {
+    const resetButton = document.createElement("button");
+    const resetText = document.createTextNode("Reset Playlist");
+
+    resetButton.id = "reset";
+    resetButton.ariaLabel = "Reset playlist";
+    resetButton.appendChild(resetText);
+    playlistSongs.appendChild(resetButton);
+
+    resetButton.addEventListener("click", () => {
+      userData.songs = [...allSongs];
+
+      renderSongs(sortSongs()); 
+      setPlayButtonAccessibleText();
+      resetButton.remove();
+    });
+
+  }
+
+};
+
+const setPlayerDisplay = () => {
+  const playingSong = document.getElementById("player-song-title");
+  const songArtist = document.getElementById("player-song-artist");
+  const currentTitle = userData?.currentSong?.title;
+  const currentArtist = userData?.currentSong?.artist;
+
+  playingSong.textContent = currentTitle ? currentTitle : "";
+  songArtist.textContent = currentArtist ? currentArtist : "";
+};
+
+const highlightCurrentSong = () => {
+  const playlistSongElements = document.querySelectorAll(".playlist-song");
+  const songToHighlight = document.getElementById(
+    `song-${userData?.currentSong?.id}`
+  );
+
+  playlistSongElements.forEach((songEl) => {
+    songEl.removeAttribute("aria-current");
+  });
+
+  if (songToHighlight) songToHighlight.setAttribute("aria-current", "true");
+};
+
+const renderSongs = (array) => {
+  const songsHTML = array
+    .map((song)=> {
+      return `
+      <li id="song-${song.id}" class="playlist-song">
+      <button class="playlist-song-info" onclick="playSong(${song.id})">
+          <span class="playlist-song-title">${song.title}</span>
+          <span class="playlist-song-artist">${song.artist}</span>
+          <span class="playlist-song-duration">${song.duration}</span>
+      </button>
+      <button onclick="deleteSong(${song.id})" class="playlist-song-delete" aria-label="Delete ${song.title}">
+          <svg width="20" height="20" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg"><circle cx="8" cy="8" r="8" fill="#4d4d62"/>
+          <path fill-rule="evenodd" clip-rule="evenodd" d="M5.32587 5.18571C5.7107 4.90301 6.28333 4.94814 6.60485 5.28651L8 6.75478L9.39515 5.28651C9.71667 4.94814 10.2893 4.90301 10.6741 5.18571C11.059 5.4684 11.1103 5.97188 10.7888 6.31026L9.1832 7.99999L10.7888 9.68974C11.1103 10.0281 11.059 10.5316 10.6741 10.8143C10.2893 11.097 9.71667 11.0519 9.39515 10.7135L8 9.24521L6.60485 10.7135C6.28333 11.0519 5.7107 11.097 5.32587 10.8143C4.94102 10.5316 4.88969 10.0281 5.21121 9.68974L6.8168 7.99999L5.21122 6.31026C4.8897 5.97188 4.94102 5.4684 5.32587 5.18571Z" fill="white"/></svg>
+        </button>
+      </li>
+      `;
+    })
+    .join("");
+
+  playlistSongs.innerHTML = songsHTML;
+};
+
+const setPlayButtonAccessibleText = () => {
+  const song = userData?.currentSong || userData?.songs[0];
+
+  playButton.setAttribute(
+    "aria-label",
+    song?.title ? `Play ${song.title}` : "Play"
+  );
+};
+
+const getCurrentSongIndex = () => userData?.songs.indexOf(userData?.currentSong);
+
+playButton.addEventListener("click", () => {
+    if (userData?.currentSong === null) {
+    playSong(userData?.songs[0].id);
+  } else {
+    playSong(userData?.currentSong.id);
+  }
 });
 
-function createTranslationMatrix(tx, ty) {
-    return new Float32Array([
-        1, 0, 0, 0,
-        0, 1, 0, 0,
-        0, 0, 1, 0,
-        tx, ty, 0, 1
-    ]);
-}
-/////////////////////////////////////////////////////////////////////////////////////////////
-function createRotationMatrixX(angleInRadians) {
-    const xcos = Math.cos(angleInRadians);
-    const xsin = Math.sin(angleInRadians);
-    return new Float32Array([
-        1, 0,     0,    0,
-        0, xcos, -xsin, 0,
-        0, xsin,  xcos, 0,
-        0, 0,     0,    1
-    ]);
-}
-/////////////////////////////////////////////////////////////////////////////////////////////
-function createRotationMatrixY(angleInRadians) {
-    const ycos = Math.cos(angleInRadians);
-    const ysin = Math.sin(angleInRadians);
-    return new Float32Array([
-        ycos,  0,  ysin, 0,
-        0,     1,  0,    0,
-        -ysin, 0,  ycos, 0,
-        0,     0,  0,    1
-    ]);
-}
-/////////////////////////////////////////////////////////////////////////////////////////////
-function createRotationMatrixZ(angleInRadians) {
-    const zcos = Math.cos(angleInRadians);
-    const zsin = Math.sin(angleInRadians);
-    return new Float32Array([
-        zcos, -zsin, 0, 0,
-        zsin, zcos,  0, 0,
-        0,   0,    1, 0,
-        0,   0,    0, 1
-    ]);
-}
-/////////////////////////////////////////////////////////////////////////////////////////////
-function createScaleMatrix(sx, sy) {
-    return new Float32Array([
-        sx, 0,  0, 0,
-        0, sy,  0, 0,
-        0, 0,  1, 0,
-        0, 0,  0, 1
-    ]);
-}
-/////////////////////////////////////////////////////////////////////////////////////////////
-function createOrthographicMatrix(left, right, bottom, top, near, far) {
-    return new Float32Array([
-        2 / (right - left), 0, 0, 0,
-        0, 2 / (top - bottom), 0, 0,
-        0, 0, -2 / (far - near), 0,
-        -(right + left) / (right - left), -(top + bottom) / (top - bottom), -(far + near) / (far - near), 1
-    ]);
-}
-/////////////////////////////////////////////////////////////////////////////////////////////
-function multiplyMatrices(a, b) {
-    const result = new Float32Array(16);
-    for (let row = 0; row < 4; row++) {
-        for (let col = 0; col < 4; col++) {
-            result[row * 4 + col] =
-                a[row * 4 + 0] * b[0 * 4 + col] +
-                a[row * 4 + 1] * b[1 * 4 + col] +
-                a[row * 4 + 2] * b[2 * 4 + col] +
-                a[row * 4 + 3] * b[3 * 4 + col];
-        }
+pauseButton.addEventListener("click",  pauseSong);
+
+nextButton.addEventListener("click", playNextSong);
+
+previousButton.addEventListener("click", playPreviousSong);
+
+shuffleButton.addEventListener("click", shuffle);
+
+audio.addEventListener("ended", () => {
+  const currentSongIndex = getCurrentSongIndex();
+  const nextSongExists = userData?.songs[currentSongIndex + 1] !== undefined;
+
+    if (nextSongExists) {
+      playNextSong();
+    } else {
+      userData.currentSong = null;
+      userData.songCurrentTime = 0;  
+pauseSong();
+setPlayerDisplay();
+highlightCurrentSong();
+setPlayButtonAccessibleText();
+    }
+});
+
+const sortSongs = () => {
+  userData?.songs.sort((a,b) => {
+    if (a.title < b.title) {
+      return -1;
     }
 
-    return result;
-    
-}
+    if (a.title > b.title) {
+      return 1;
+    }
+
+    return 0;
+  });
+
+  return userData?.songs;
+};
+
+renderSongs(sortSongs());
+setPlayButtonAccessibleText();
